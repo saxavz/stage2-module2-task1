1fea7fc2a1734739158db5f1bdc3c1222c335f6c
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-04-15 22:12:10 +0200

    done

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,32 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+
+        if(firstName != null && lastName != null) {
+            User u = new User(firstName, lastName);
+            Warehouse.getInstance().addUser(u);
+            req.setAttribute("user", u);
+        }
+        req.getRequestDispatcher("jsp/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 }
